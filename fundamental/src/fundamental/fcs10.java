16eb5c114dd11945addd631cd5dc6131735a8b04
hi <hi@DESKTOP-ATOAOO5>
2019-06-26 11:38:28 +0530

    version 1

---
+++ fundamental/src/fundamental/fcs10.java
@@ -0,0 +1,8 @@
+package fundamental;
+
+public class fcs10 {
+	for(int i=1;i<=10;i++)
+	    System.out.print(i+"\t");
+	 }
+
+}
---
+++ fundamental/src/fundamental/fcs2.java
@@ -0,0 +1,14 @@
+package fundamental;
+import java.util.Scanner;
+public class fcs2
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+	}
+}
