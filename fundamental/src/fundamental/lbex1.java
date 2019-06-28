1f4c998c04812a0ac4c15b03558bc3a339150b89
hi <hi@DESKTOP-ATOAOO5>
2019-06-28 10:48:40 +0530

    version3

---
+++ fundamental/src/fundamental/lbex1.java
@@ -0,0 +1,8 @@
+package fundamental;
+
+public class lbex1 {
+	public static void main(String args[])
+	{
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
+}
---
+++ fundamental/src/fundamental/lbex3.java
@@ -0,0 +1,9 @@
+package fundamental;
+
+public class lbex3 {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+a);
+	}
+}
